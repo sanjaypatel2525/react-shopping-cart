d56384cef9a865487e37b94befdf3ef6df0668c8
sanjay patel <sanjaypatel2525@gmail.com>
2021-02-27 19:01:32 -0500

    first version

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sample react-shopping-cart
---
+++ src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping</a>
+      </header>
+      <main>
+        Product list
+      </main>
+      <footer>
+        all rights reserved
+      </footer>
+    </div>
+  );
+}
+
+export default App;
